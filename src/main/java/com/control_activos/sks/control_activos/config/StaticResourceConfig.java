c80d93326e9187c054c92fab13d28a533662949e
Jaime-vargas <jaime.vargas@live.com.mx>
2026-08-04 12:33:17 -0600

    improve handle of static resources

--- src/main/java/com/control_activos/sks/control_activos/config/StaticResourceConfig.java
+++ src/main/java/com/control_activos/sks/control_activos/config/StaticResourceConfig.java
@@ -1,16 +1,22 @@
 package com.control_activos.sks.control_activos.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.nio.file.Path;
+
 @Configuration
 public class StaticResourceConfig implements WebMvcConfigurer {
 
-    private static final String UPLOAD_DIR = "file:uploads/";
+    @Value("${app.storage.base-path}")
+    private String storagePath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String location = Path.of(storagePath).toUri().toString();
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations(UPLOAD_DIR);
+                .addResourceLocations(location);
     }
 }
