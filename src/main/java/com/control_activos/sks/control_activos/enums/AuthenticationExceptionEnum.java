64c376cf11fb94229019b03a0ecca304007c1e1d
Jaime-vargas <jaime.vargas@live.com.mx>
2026-08-04 12:35:50 -0600

    added new error on Authentication exception enum

--- src/main/java/com/control_activos/sks/control_activos/enums/AuthenticationExceptionEnum.java
+++ src/main/java/com/control_activos/sks/control_activos/enums/AuthenticationExceptionEnum.java
@@ -10,6 +10,7 @@ public enum AuthenticationExceptionEnum {
     TOKEN_INVALID("Invalid authentication token"),
 
     // Permissions and roles;
+    NOT_REGISTERED_USER("The request, can't be processed without an user."),
     FORBIDDEN_ACCESS("You do not have permission to access this resource");
 
     private final String message;
