0252e05a39f42507abe7613253ec11160c977c6e
Jaime-vargas <jaime.vargas@live.com.mx>
2026-08-04 12:31:28 -0600

    fixed improvements on rutes

--- src/main/java/com/control_activos/sks/control_activos/config/SecurityConfiguration.java
+++ src/main/java/com/control_activos/sks/control_activos/config/SecurityConfiguration.java
@@ -3,14 +3,21 @@ package com.control_activos.sks.control_activos.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import java.util.List;
+
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
@@ -29,11 +36,12 @@ public class SecurityConfiguration {
                 // ACCESS RULES
             .authorizeHttpRequests(auth -> auth
                     //temp for view images
+
                     .requestMatchers("/uploads/**").permitAll()
-                    .requestMatchers("/api/v1/**").permitAll()
-                    .requestMatchers("/test/public").permitAll()
+                    .requestMatchers("/api/v1/login").permitAll()
+                    .requestMatchers("/api/v1/**").authenticated()
+                    .requestMatchers("/**").permitAll()
                     .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
-                    .anyRequest().permitAll()
             ).addFilterBefore(
                 authenticationFilter, UsernamePasswordAuthenticationFilter.class
                 );
@@ -48,6 +56,7 @@ public class SecurityConfiguration {
                 corsConfig.setAllowedOrigins(java.util.List.of("*"));
                 corsConfig.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
                 corsConfig.setAllowedHeaders(java.util.List.of("*"));
+                corsConfig.setExposedHeaders(List.of(HttpHeaders.CONTENT_DISPOSITION));
                 return corsConfig;
             })
         );
